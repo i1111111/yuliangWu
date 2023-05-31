4a551b90724e89501a3929853fef033dc387a7ab
student <s1101702@gm.pu.edu.tw>
2023-05-31 10:46:29 +0800

    Commitv.2

--- app/src/main/java/com/example/yuliangwu/MainActivity.kt
+++ app/src/main/java/com/example/yuliangwu/MainActivity.kt
@@ -1,16 +1,27 @@
 package com.example.yuliangwu
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.yuliangwu.R.drawable
 import com.example.yuliangwu.ui.theme.YuliangWuTheme
+import androidx.compose.foundation.Image as Image
+import androidx.compose.material.Text as Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,8 +39,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = "作者：資管二A 吳育樑")
+        Image(
+            painter = painterResource(id = R.drawable.map),
+            contentDescription = "圖片",
+            modifier = Modifier
+        )
+
+    }
+
 }
+
+
 
 @Preview(showBackground = true)
 @Composable
@@ -38,3 +60,6 @@ fun DefaultPreview() {
         Greeting("Android")
     }
 }
+
+
+
